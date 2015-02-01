523bd1b0bc00eded84b29a0e49272bd2a9450803
Dejan Stojanovic <d.stojanovic@hotmail.com>
2015-02-01 16:52:14 +0400

    Added project template and VSIX extension projects

---
+++ Application.Plugins.ProjectTemplate/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $safeprojectname$
+{
+	public class Plugin:Application.Plugins.Plugin
+	{
+		 public Plugin(string assemblyPath)
+			: base(assemblyPath)
+		{
+			//Add constructor logic here
+		}
+	}
+}
