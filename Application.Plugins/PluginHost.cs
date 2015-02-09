ec094abbb31d5f8b803d6a2f1e83dd19aff27245
Dejan Stojanovic <d.stojanovic@hotmail.com>
2015-02-09 21:39:19 +0400

    Added plugin host abstract class

---
+++ Application.Plugins/PluginHost.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Plugins
+{
+    /// <summary>
+    /// Provides base class with singletone PluginManager instance
+    /// </summary>
+    public virtual class PluginHost
+    {
+        private static PluginManager pluginManager;
+
+        /// <summary>
+        /// Singletone PluginManager instance
+        /// </summary>
+        public static PluginManager PluginManager
+        {
+            get
+            {
+                if (PluginManager != null)
+                {
+                    pluginManager = new PluginManager();
+                }
+                return pluginManager;
+            }
+        }
+    }
+}
